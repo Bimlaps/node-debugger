9bab9d937b2f9154f21b07bf44a4bc97b273138c
Tarik <t.saadaoui.ts@gmail.com>
2024-10-29 23:18:36 +0100

    feat: commit message here

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // index.js
-var items = [1, "2", 3, "four", 5, null, undefined, 6]
-const results = []
+var items = [1, "2", 3, "four", 5, null, undefined, 6];
+const results = [];
 
-items.foreach((item) => {
-    if(typeof item === 'number')
-        if(item % 2 === 0) results.push(item* 2)
-
-    else if(item === null)
-    console.log("Found a null item")
-
-    else 
-    {
-        result.push(parseInt(item))
+// Verwendung forEach anstelle von foreach
+items.forEach((item) => {
+    if (typeof item === 'number') {
+        if (item % 2 === 0) {
+            results.push(item * 2);
+        }
+    } else if (item === null) {
+        console.log("Found a null item");
+    } else {
+        // Korrekture result zu results
+        results.push(parseInt(item));
     }
 });
 
-console.log("Original items:", items)
-console.log("Filtered and transformed items:", results)
+console.log("Original items:", items);
+console.log("Filtered and transformed items:", results);
+console.log("Hello world");
+
